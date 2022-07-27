e91bf0eee1f0caa90b4ebb8e823ce85bc6544cfe
MiroslavGit <83980350+MiroslavGit@users.noreply.github.com>
2022-07-27 23:24:04 +0200

    conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>44444444444444444444444444444444444444444444</p>
+        <p>888888888888888888888</p>
         <a
           className="App-link"
           href="https://reactjs.org"
