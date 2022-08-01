de084994880050015a07681bb6bd9341957c84b1
MiroslavGit <83980350+MiroslavGit@users.noreply.github.com>
2022-08-01 22:07:09 +0200

    redux deleted

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-/* import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
-import allReducers from "./reducers/index";
-
-const store = createStore(allReducers); */
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
