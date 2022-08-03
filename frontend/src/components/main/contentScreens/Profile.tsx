72df6bc3c938ae115cf521a78a9471ceb7a59db2
MiroslavGit <83980350+MiroslavGit@users.noreply.github.com>
2022-08-03 20:14:09 +0200

    added loading

--- frontend/src/components/main/contentScreens/Profile.tsx
+++ frontend/src/components/main/contentScreens/Profile.tsx
@@ -4,6 +4,7 @@ import "./profile.css";
 
 import Input from "../../../const/Input";
 import Button from "@mui/material/Button";
+import { Oval } from "react-loader-spinner";
 
 import { getUserFetch } from "../../../actions/getUserFetch";
 import { deleteUserFetch } from "../../../actions/deleteUserFetch";
@@ -27,91 +28,110 @@ export default function Profile() {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [copyNewPassword, setCopyNewPassword] = useState("");
+  const [spinner, setSpinner] = useState(false);
 
   useEffect(() => {
     getUserData();
   }, []);
 
   const getUserData = async () => {
+    setSpinner(true);
     const user = await getUserFetch(String(userID));
     setUserData(user);
+    setSpinner(false);
   };
 
   return (
     <>
       <h2 className="title">Tvoj profil</h2>
-      <div className="container">
-        <div className="profileDiv">
-          <h1 className="containerTitle">Údaje</h1>
-          <p className="dataName">Celé meno</p>
-          <div className="flex-container">
-            <p className="dataLabel">{userData?.firstName}</p>
-            <p style={{ margin: "10px" }}></p>
-            <p className="dataLabel">{userData?.lastName}</p>
+      {spinner && (
+        <Oval
+          height={80}
+          width={80}
+          color="#4fa94d"
+          wrapperStyle={{ justifyContent: "center", marginTop: "100px" }}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor="#4fa94d"
+          strokeWidth={2}
+          strokeWidthSecondary={2}
+        />
+      )}
+      {!spinner && (
+        <div className="container">
+          <div className="profileDiv">
+            <h1 className="containerTitle">Údaje</h1>
+            <p className="dataName">Celé meno</p>
+            <div className="flex-container">
+              <p className="dataLabel">{userData?.firstName}</p>
+              <p style={{ margin: "10px" }}></p>
+              <p className="dataLabel">{userData?.lastName}</p>
+            </div>
+            <p className="dataName">Email</p>
+            <p className="dataLabel">{userData?.email}</p>
+            <p className="dataName">Používateľské meno</p>
+            <p className="dataLabel">{userData?.username}</p>
+            <p className="dataName">Dátum vytvorenia:</p>
+            <p className="dataLabel">
+              {formatDate(String(userData?.registeredAt))}
+            </p>
           </div>
-          <p className="dataName">Email</p>
-          <p className="dataLabel">{userData?.email}</p>
-          <p className="dataName">Používateľské meno</p>
-          <p className="dataLabel">{userData?.username}</p>
-          <p className="dataName">Dátum vytvorenia:</p>
-          <p className="dataLabel">
-            {formatDate(String(userData?.registeredAt))}
-          </p>
+          <div className="passwordDiv">
+            <h1 className="containerTitle">Heslo</h1>
+            <Input
+              name="oldPassword"
+              label="Staré heslo"
+              type="password"
+              handleChange={(e: any) => {
+                setOldPassword(e.target.value);
+              }}
+              value={oldPassword}
+            />
+            <div className="separator"></div>
+            <Input
+              name="newPassword"
+              label="Nové heslo"
+              type="password"
+              handleChange={(e: any) => {
+                setNewPassword(e.target.value);
+              }}
+              value={newPassword}
+            />
+            <div className="separator"></div>
+            <Input
+              name="copyNewPassword"
+              label="Potvrdenie nového hesla "
+              type="password"
+              handleChange={(e: any) => {
+                setCopyNewPassword(e.target.value);
+              }}
+              value={copyNewPassword}
+            />
+            <div className="separator"></div>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Potvrdiť zmeny
+            </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, backgroundColor: "red" }}
+              onClick={async () => {
+                localStorage.clear();
+                await deleteUserFetch(String(userID), navigate);
+              }}
+            >
+              Nenávratne zmazať užívateľa
+            </Button>
+          </div>
         </div>
-        <div className="passwordDiv">
-          <h1 className="containerTitle">Heslo</h1>
-          <Input
-            name="oldPassword"
-            label="Staré heslo"
-            type="password"
-            handleChange={(e: any) => {
-              setOldPassword(e.target.value);
-            }}
-            value={oldPassword}
-          />
-          <div className="separator"></div>
-          <Input
-            name="newPassword"
-            label="Nové heslo"
-            type="password"
-            handleChange={(e: any) => {
-              setNewPassword(e.target.value);
-            }}
-            value={newPassword}
-          />
-          <div className="separator"></div>
-          <Input
-            name="copyNewPassword"
-            label="Potvrdenie nového hesla "
-            type="password"
-            handleChange={(e: any) => {
-              setCopyNewPassword(e.target.value);
-            }}
-            value={copyNewPassword}
-          />
-          <div className="separator"></div>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Potvrdiť zmeny
-          </Button>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2, backgroundColor: "red" }}
-            onClick={async () => {
-              localStorage.clear();
-              await deleteUserFetch(String(userID), navigate);
-            }}
-          >
-            Nenávratne zmazať užívateľa
-          </Button>
-        </div>
-      </div>
+      )}
     </>
   );
 }
