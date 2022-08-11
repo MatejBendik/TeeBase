ad364c07512b1fac02ed26d1c0968fc65fc4625c
MiroslavGit <83980350+MiroslavGit@users.noreply.github.com>
2022-08-11 21:26:33 +0200

    idk fakt ale

--- frontend/src/screens/authScreens/Login.tsx
+++ frontend/src/screens/authScreens/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 import Button from "@mui/material/Button";
